259b21557a73421556f13425632e3680ee9c3a9b
Denzel <okungbowadenzel65@gmail.com>
2025-12-10 17:26:38 +0100

    fix: resolve startup issue by changing the imported Springdoc Class "OpenApiCustomiser" to "OpenApiCustomizer"

--- src/main/java/com/wallet/config/SwaggerConfig.java
+++ src/main/java/com/wallet/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.wallet.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
-    public OpenApiCustomiser globalHeaderOpenApiCustomiser() {
+    public OpenApiCustomizer globalHeaderOpenApiCustomiser() {
         return openApi -> {
             // Add JWT security scheme
             openApi.getComponents()
