701d0e94696d9976e2c78b14456829631073d7c6
mydas <106563584+mydas-denzel@users.noreply.github.com>
2025-12-10 15:16:50 +0100

    chore: create SwaggerConfig.java

---
+++ src/main/java/com/wallet/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.wallet.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenApiCustomiser globalHeaderOpenApiCustomiser() {
+        return openApi -> {
+            // Add global security scheme
+            openApi.getComponents()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                );
+
+            // Apply security scheme globally
+            openApi.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+        };
+    }
+}
